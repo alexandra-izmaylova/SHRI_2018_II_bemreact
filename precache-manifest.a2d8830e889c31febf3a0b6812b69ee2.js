42f1c022e92d369ec1fee579161698f4dae78be3
Александра <>
2018-11-13 22:09:15 +0500

    Initial build subtree commit

---
+++ precache-manifest.a2d8830e889c31febf3a0b6812b69ee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ef9c46fe9808e6e32e4004b940d5d53",
+    "url": "/static/media/logo.2ef9c46f.svg"
+  },
+  {
+    "revision": "ce4de541bca65a00ff70736d9ac39671",
+    "url": "/static/media/icon_list_m@1x.ce4de541.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "152cea80e9dd0dfe9ce1",
+    "url": "/static/js/main.152cea80.chunk.js"
+  },
+  {
+    "revision": "345daaa155932f368895",
+    "url": "/static/js/1.345daaa1.chunk.js"
+  },
+  {
+    "revision": "152cea80e9dd0dfe9ce1",
+    "url": "/static/css/main.d25d88ad.chunk.css"
+  },
+  {
+    "revision": "5a66c7b8d31690e0e1119d476be15c7b",
+    "url": "/index.html"
+  }
+];
