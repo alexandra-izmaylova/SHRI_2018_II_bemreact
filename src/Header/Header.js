65e01b9be7b12711991ce4350d1637edbecc4adb
Александра <>
2018-11-13 19:41:00 +0500

    +header & footer

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Header.css';
+import { cn } from '@bem-react/classname';
+
+const logo = cn('Logo');
+const menuIcon = cn('MenuIcon');
+const menu = cn('Menu');
 
 function Header() {
     const list = ['События','Сводка','Устройства','Сценарии'];
     const listItems = list.map(elem => <li key={elem}><a href="/">{elem}</a></li>);
     return (
         <header>
-            <div className="logo"></div>
-            <label htmlFor="menu" className="menu_icon"></label>
-            <input type="checkbox" className="menu" defaultChecked={false} id="menu"></input>
+            <div className={logo()}></div>
+            <label htmlFor="menu" className={menuIcon()}></label>
+            <input type="checkbox" className={menu()} defaultChecked={false} id="menu"></input>
             <nav>
                 <ul>
                     {listItems}
